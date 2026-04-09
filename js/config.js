0ac5d8b4db36b0c524953b5ab9cdd988d74fde21
kometaro4649 <you2.209612@gmail.com>
2026-04-09 18:21:12 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,24 +3,26 @@
 // Firebase コンソール (https://console.firebase.google.com) で
 // プロジェクトを作成し、以下の値を置き換えてください
 // ============================================================
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBJiL26c9nvZNyjKkEcoWHFaZQR2VSxVuQ",
-  authDomain: "kometaro-x.firebaseapp.com",
-  projectId: "kometaro-x",
-  storageBucket: "kometaro-x.firebasestorage.app",
-  messagingSenderId: "889271140667",
-  appId: "1:889271140667:web:83bbdcbe5af10d5509371e",
-  measurementId: "G-YN7KT387WF"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBJiL26c9nvZNyjKkEcoWHFaZQR2VSxVuQ",
+    authDomain: "kometaro-x.firebaseapp.com",
+    projectId: "kometaro-x",
+    storageBucket: "kometaro-x.firebasestorage.app",
+    messagingSenderId: "889271140667",
+    appId: "1:889271140667:web:9d6d2c843db81ad209371e",
+    measurementId: "G-XTRHRDWKE5"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
