c2926e01924423c5da44974459d30d26bf2ee42c
kometaro4649 <you2.209612@gmail.com>
2026-04-09 18:25:58 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,28 +1,24 @@
-// ============================================================
-// Firebase 設定ファイル
-// Firebase コンソール (https://console.firebase.google.com) で
-// プロジェクトを作成し、以下の値を置き換えてください
-// ============================================================
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
+// Auth と Firestore も追加
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBJiL26c9nvZNyjKkEcoWHFaZQR2VSxVuQ",
-    authDomain: "kometaro-x.firebaseapp.com",
-    projectId: "kometaro-x",
-    storageBucket: "kometaro-x.firebasestorage.app",
-    messagingSenderId: "889271140667",
-    appId: "1:889271140667:web:9d6d2c843db81ad209371e",
-    measurementId: "G-XTRHRDWKE5"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBJiL26c9nvZNyjKkEcoWHFaZQR2VSxVuQ",
+  authDomain: "kometaro-x.firebaseapp.com",
+  projectId: "kometaro-x",
+  storageBucket: "kometaro-x.firebasestorage.app",
+  messagingSenderId: "889271140667",
+  appId: "1:889271140667:web:9d6d2c843db81ad209371e",
+  measurementId: "G-XTRHRDWKE5"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, analytics, auth, db };
