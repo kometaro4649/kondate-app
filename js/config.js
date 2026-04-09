828a31d374663568476c767119e2f458f7638e69
kometaro4649 <you2.209612@gmail.com>
2026-04-09 20:17:59 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
-// Auth と Firestore も追加
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
-
-const firebaseConfig = {
+window.firebaseConfig = {
   apiKey: "AIzaSyBJiL26c9nvZNyjKkEcoWHFaZQR2VSxVuQ",
   authDomain: "kometaro-x.firebaseapp.com",
   projectId: "kometaro-x",
@@ -14,11 +7,3 @@ const firebaseConfig = {
   appId: "1:889271140667:web:9d6d2c843db81ad209371e",
   measurementId: "G-XTRHRDWKE5"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { app, analytics, auth, db };
